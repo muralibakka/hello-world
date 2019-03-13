8670cc333cee96213b907c30a2fcea4f4d5b83b4
muralibakka <bakkamuralione@gmail.com>
2019-03-13 16:08:49 +0530

    first commit without .env file

---
+++ app/Http/Controllers/CurltestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Curltest;
+use Illuminate\Http\Request;
+
+class CurltestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Curltest  $curltest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Curltest $curltest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Curltest  $curltest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Curltest $curltest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Curltest  $curltest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Curltest $curltest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Curltest  $curltest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Curltest $curltest)
+    {
+        //
+    }
+}
